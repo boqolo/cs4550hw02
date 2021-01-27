488f920e60f321ef15d119bc05a1a554a8122281
rayyan <rayyan10@bellsouth.net>
2021-01-27 16:22:27 -0600

    finish calc

--- calc.js
+++ calc.js
@@ -1,3 +1,131 @@
-// Calculator implementation
+/*** Calculator implementation ***/
 
+// Init once the page has loaded
+window.onload = () => {
+  "use strict";
+
+  // State
+  let state = {
+    numStr: "",
+    fn: undefined,
+    isResult: false
+  };
+
+  // DOM update fn
+  var updateDOM = (displayStr) => document.getElementById("display").innerHTML = displayStr;
+
+  // Digit Button Handler
+  var pressDigit = (digitStr) => {
+    var isOperator = /[-\+/\*]/.test(state.numStr);
+    if (state.isResult || isOperator) {
+      state.numStr = "";
+      state.isResult = false;
+    }
+    state.numStr = state.numStr.concat(digitStr);
+    updateDOM(state.numStr);
+  }
+
+  // ----- Bind Digit Button Handlers
+  document.getElementById("1").onclick = () => pressDigit("1");
+  document.getElementById("2").onclick = () => pressDigit("2");
+  document.getElementById("3").onclick = () => pressDigit("3");
+  document.getElementById("4").onclick = () => pressDigit("4");
+  document.getElementById("5").onclick = () => pressDigit("5");
+  document.getElementById("6").onclick = () => pressDigit("6");
+  document.getElementById("7").onclick = () => pressDigit("7");
+  document.getElementById("8").onclick = () => pressDigit("8");
+  document.getElementById("9").onclick = () => pressDigit("9");
+  document.getElementById("0").onclick = () => {
+    if (state.numStr) {
+      pressDigit("0");
+    }
+  };
+
+  // Decimal point Handler
+  document.getElementById("decimal").onclick = () => {
+    if (!state.numStr.includes(".")) {
+      state.numStr = state.numStr.concat(".");
+      updateDOM(state.numStr);
+    }
+  };
+
+  // ----- Function Button Handlers
+  document.getElementById("plus/equals").onclick = () => {
+    if (!state.numStr) {
+      return;
+    }
+    else if (state.fn) {
+      var calcSoFar = state.fn(parseFloat(state.numStr)).toString();
+      state.fn = undefined;
+      state.isResult = true;
+      state.numStr = calcSoFar;
+      updateDOM(calcSoFar);
+    } else {
+      var calcSoFar = state.numStr;
+      state.fn = (arg) => parseFloat(calcSoFar) + arg;
+      state.numStr = "+";
+      updateDOM("+");
+    }
+  };
+
+  document.getElementById("minus").onclick = () => {
+    if (!state.numStr) {
+      return;
+    }
+    else if (state.fn) {
+      var calcSoFar = state.fn(parseFloat(state.numStr)).toString();
+      state.fn = (arg) => parseFloat(calcSoFar) - arg;
+      state.numStr = "-";
+      updateDOM(calcSoFar);
+    } else {
+      var calcSoFar = state.numStr;
+      state.fn = (arg) => parseFloat(calcSoFar) - arg;
+      state.numStr = "-";
+      updateDOM("-");
+    }
+  };
+
+  document.getElementById("times").onclick = () => {
+    if (!state.numStr) {
+      return;
+    }
+    else if (state.fn) {
+      var calcSoFar = state.fn(parseFloat(state.numStr)).toString();
+      state.fn = (arg) => parseFloat(calcSoFar) * arg;
+      state.numStr = "*";
+      updateDOM(calcSoFar);
+    } else {
+      var calcSoFar = state.numStr;
+      state.fn = (arg) => parseFloat(calcSoFar) * arg;
+      state.numStr = "*";
+      updateDOM("*");
+    }
+  };
+
+  document.getElementById("divide").onclick = () => {
+    if (!state.numStr) {
+      return;
+    }
+    else if (state.fn) {
+      var calcSoFar = state.fn(parseFloat(state.numStr)).toString();
+      state.fn = (arg) => parseFloat(calcSoFar) / arg;
+      state.numStr = "/";
+      updateDOM(calcSoFar);
+    } else {
+      var calcSoFar = state.numStr;
+      state.fn = (arg) => parseFloat(calcSoFar) / arg;
+      state.numStr = "/";
+      updateDOM("/");
+    }
+  };
+
+  // ----- Clear Button Handler
+  document.getElementById("clear").onclick = () => {
+    // reset state
+    state.numStr = "";
+    state.fn = undefined;
+    state.isResult = false;
+    updateDOM("0");
+  };
+};
 
